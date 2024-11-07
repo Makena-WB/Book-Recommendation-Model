d710fa4e4ead9a4c7d42851776a074de8f3ad0a4
Makena-WB <makenawahu@gmail.com>
2024-11-07 15:26:09 +0300

    styles

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router';
+// import { StyleSheet, Text, View } from 'react-native'
+// import { Slot, Stack } from 'expo-router';
 
-const RootLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  )     
-}
+// const RootLayout = () => {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="index" options={{ headerShown: false }} />
+//     </Stack>
+//   )     
+// }
 
-export default RootLayout
+// export default RootLayout
+
+import { Slot } from "expo-router";
+
+// Import your global CSS file
+import "/styles/global.css";
+
+export default Slot;
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -6,8 +6,8 @@ import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl">ShelfScape</Text>
+    <View /*className="flex-1 items-center justify-center bg-white"*/>
+        <Text /*className="text-3xl"*/>ShelfScape</Text>
         <StatusBar style = "auto" />
         <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
