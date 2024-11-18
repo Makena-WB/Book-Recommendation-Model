fb1606b6bcfa639fff7a93a71569ca6c203a8b64
Makena-WB <makenawahu@gmail.com>
2024-11-18 19:51:46 +0300

    dependencies

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,10 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="expo">
       <Text className="text-3xl">ShelfScape</Text>
       <StatusBar style="auto"/>
       <Link href={"/profile"} style={{ color: 'blue'}}>Go to Profile</Link>
