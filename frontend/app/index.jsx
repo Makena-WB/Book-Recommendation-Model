6b08371eb7996dcb7f4867d2e02777e77973c8d2
Makena-WB <makenawahu@gmail.com>
2024-12-27 21:48:40 +0300

    onboarding screen

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,19 +1,32 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 import { images } from '../constants';
+
+
 const index = () => {
     return (
         <SafeAreaView className='bg-primary h-full'>
             <ScrollView contentContainerStyle={{ height: '100%' }}>
                 <View className="w-full justify-center items-center h-full px-4">
                     <Image
-                    source={images.logo}
-                    className="w-[130px] h-[84px]"
-                    reszieMode="contain"
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode="contain"
                     />
+
+                    <Image
+                        source={images.cards}
+                        className="max-w-[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5" >
+                        <Text className="text-3xl text-white font-serif font-pthin text-center">"In the end, we all become stories"</Text>
+
+                    </View>
 
                 </View>
             </ScrollView>
